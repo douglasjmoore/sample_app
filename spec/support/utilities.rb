88b50bcfd8183478578f3201572426e2f5e9a2c4
Doug Moore <douglas_j_moore@mail.com>
2014-03-13 19:36:27 -0400

    Chapter9-Start-updating-users Done Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,12 +6,26 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
 
 def valid_signup
-	fill_in "Name",         with: "Example User"
-    fill_in "Email",        with: "user@example.com"
-    fill_in "Password",     with: "foobar"
-    fill_in "Confirmation", with: "foobar"
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
@@ -20,7 +34,7 @@ RSpec::Matchers.define :have_error_message do |message|
   end
 end
 
-
+# Updates from listing 9.6/
 #def full_title(page_title)
 #  base_title = "Ruby on Rails Tutorial Sample App"
 #  if page_title.empty?
